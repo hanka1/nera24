493787de2c16b2b1b630512620164e48c4a52688
Hanka Jezkova <hanka.jezkova@email.cz>
2025-01-22 12:31:28 +0100

    history

--- docs/public/data/history_info.js
+++ docs/public/data/history_info.js
@@ -1,18 +1,23 @@
 const history_info = {
-    cz: `TODO Historie závodu
-Jsme parta nadšenců, kteří rádi sportují a nebojí se postavit výzvám. Začínali jsme se Spartan běhy, posléze jsme se účastnili trailových a ultramaratonských běhů, kterým se někteří věnují dodnes.
-Všichni se ale již několik let věnujeme paddlebordingu, kde jsme postupně z nafukovacích prken přešli na karbonová,  pravidelně účastníme závodů ČSFSA.
-Jako 1. tým z ČR jsme se v roce 2023 zúčastnili štafetového závodu 11 City tour v Nizozemí.
-Náš handicepovaný člen jel dokonce tento závod jako sólo jezdec.
-
-První ročník neratovické čtyřiadvacetihodinovky se konal v září 2021, kdy klub SK LiveCross Neratovice 
-přišel s unikátním nápadem uspořádat non-stop paddleboardový závod trvající celých 24 hodin. 
-Od té doby se tato výjimečná událost koná každý rok. Účastníci se snaží pokořit magickou hranici 200 kilometrů. Dosud se to podařilo pouze jednomu/dvěma t
-Letos se bude konat již čtvrtý ročník této unikátní akce, a to 28.-29. září 2024 na tradičním místě ve veslařské loděnici v Neratovicích. Závod si za svou krátkou historii vybudoval pevné místo v kalendáři českého paddleboardingu a ukazuje, že i v Česku máme odvážné sportovce, kteří se nebojí extrémních výzev.
-
-    
-    `,
-    en: `TODO Race history`
+    cz: `
+Závod na 24h se konal poprvé v roce 2021, kdy ještě zhruba třetina závodníků jela celý závod na nafukovacích boardech. Dnes je to už spíše výjimka.
+<br><br>
+Od roku 2023 se závodu  pravidelně účastní i naši hendikepovaní kamarádi. Mají náš velký respekt, neboť vždy jeli závod jako sólo jezdci.
+<br><br>
+V roce 2024 nás velmi potrápily povodně, které byly na začátku září a postihly nejvíce Moravu. Do poslední chvíle jsme nevěděli, zda nám povodí Labe dovolí „vstup“ na řeku. Konečné rozhodnutí padlo až den před závodem. Podmínky na řece, hlavně proud, byly obtížnější, než jiné roky, ale všichni je zvládly na výbornou.
+<br><br>
+Každým rokem se lehce zvyšuje počet účastníků a oblibu si závod nachází i u týmů (člověk se tolik nenadře a skvěle si vychutná atmosféru na vodě v zázemí :-)
+ <br><br>   
+`,
+    en: `
+The 24-hour race was first held in 2021, when about one-third of the racers completed the entire race on inflatable boards. Today, this is more of an exception.
+<br><br> 
+Since 2023, our handicapped friends have been regularly participating in the race. They have our great respect, as they always competed as solo riders.
+ <br><br> 
+In 2024, we were greatly troubled by floods that occurred in early September and mostly affected Moravia. Until the last moment, we didn't know whether the Elbe River authority would allow us "access" to the river. The final decision was made just a day before the race. The river conditions, especially the current, were more challenging than in other years, but everyone handled them excellently.
+ <br><br> 
+Each year, the number of participants slightly increases, and the race is becoming popular with teams (you don't have to work as hard, and you can perfectly enjoy the atmosphere both on the water and at the base camp :-)
+`
 } 
 
 const about_race = {
